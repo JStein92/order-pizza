aaa80733aceb59618288a481dc89b171eedc0798
JStein92 <jonathanstein@live.com>
2017-07-14 11:38:03 -0700

    let the user click through different stages with form submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,87 @@
 var crusts = ['thin','thick','stuffed'];
 var sizes = ['xtraLarge', 'large', 'medium', 'small'];
-
+var orderStage = 1;
 function Pizza(size, crust, toppings){
   this.size = size;
   this.crust = crust;
   this.toppings = [];
 }
-function hideAndShow(toShow){
-      $('#orderCrust').hide();
-      $('#orderToppings').hide();
-      $('#orderSize').hide();
+function hideAndShow(stage){
+  $('#orderCrust').hide();
+  $('#orderToppings').hide();
+  $('#orderSize').hide();
 
-      toShow.fadeIn('fast');
+  if (stage===1){
+      $('#orderSize').fadeIn('fast');
+      textColor($('#arrowOneText'));
+
+  }
+  if (stage===2){
+      $('#orderCrust').fadeIn('fast');
+        textColor($('#arrowTwoText'));
+  }
+  if (stage===3){
+      $('#orderToppings').fadeIn('fast');
+        textColor($('#arrowThreeText'));
+  }
+
+
+}
+
+function calculateTotal(pizza){
+$('#cartItems').text('');
+$('#cartItems').append("<li>" + 'Size: ' + pizza.size + "</li>")
+$('#cartItems').append("<li>" +"Crust: " + pizza.crust + "</li>")
+$('#cartItems').append("<li>" + "Toppings: "+ pizza.toppings + "</li>")
+}
+
+function textColor(idToChange){
+      $('#arrowOneText').removeClass('greenText').addClass('whiteText')
+  $('#arrowTwoText').removeClass('greenText').addClass('whiteText')
+      $('#arrowThreeText').removeClass('greenText').addClass('whiteText')
+idToChange.addClass('greenText');
 }
 
 $(function(){
-
+  orderStage=1;
+  hideAndShow(orderStage);
+  $('#arrowOneText').addClass('whiteText');
+  $('#arrowTwoText').addClass('whiteText');
+  $('#arrowThreeText').addClass('whiteText');
 $('form').submit(function(e){
   e.preventDefault();
-  var newPizza = new Pizza(sizes[0], crusts[0], Array [0])
+  var selectedSize = $('#size').val();
+  var newPizza = new Pizza(selectedSize, crusts[0], Array [0])
   $('input[type=checkbox]:checked').each(function(){
     newPizza.toppings.push(this.value);
   });
-  alert(newPizza.toppings)
+
+
+  calculateTotal(newPizza);
+
+if (orderStage<3){
+    orderStage++;
+}
+
+  hideAndShow(orderStage);
+
 })
 
 $('#arrowOne').click(function(){
-  hideAndShow($('#orderSize'));
+  orderStage = 1;
+  hideAndShow(orderStage);
+
+
 });
 $('#arrowTwo').click(function(){
-  hideAndShow($('#orderCrust'));
+  orderStage = 2;
+  hideAndShow(orderStage);
+
 });
 $('#arrowThree').click(function(){
-  hideAndShow($('#orderToppings'));
+  orderStage = 3;
+  hideAndShow(orderStage);
+
 });
 
 //  alert(newPizza.crust);
