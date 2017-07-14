a4682908c5357774097492bba8f9675808ba8d3b
JStein92 <jonathanstein@live.com>
2017-07-14 10:38:14 -0700

    add functionality to progress between order stages

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,34 @@ function Pizza(size, crust, toppings){
   this.crust = crust;
   this.toppings = [];
 }
+function hideAndShow(toShow){
+      $('#orderCrust').hide();
+      $('#orderToppings').hide();
+      $('#orderSize').hide();
+
+      toShow.fadeIn('fast');
+}
 
 $(function(){
 
 $('form').submit(function(e){
   e.preventDefault();
-
-
   var newPizza = new Pizza(sizes[0], crusts[0], Array [0])
   $('input[type=checkbox]:checked').each(function(){
     newPizza.toppings.push(this.value);
   });
-
   alert(newPizza.toppings)
 })
+
+$('#arrowOne').click(function(){
+  hideAndShow($('#orderSize'));
+});
+$('#arrowTwo').click(function(){
+  hideAndShow($('#orderCrust'));
+});
+$('#arrowThree').click(function(){
+  hideAndShow($('#orderToppings'));
+});
 
 //  alert(newPizza.crust);
 
